76b328d5d568889390f60e3bc2bbe39bc481d17d
NicksonYap <hi@nickson.me>
2019-08-28 04:46:10 +0800

    3 Base Stations - plot only

--- lighthousese3.m
+++ lighthousese3.m
@@ -1,22 +1,33 @@
 
 %% Setup
 
-O = zeros(3,3);
+O = zeros(3, 3);
 
-ID = eye(3,3);
-ID_x = ID(1,:);
-ID_y = ID(2,:);
-ID_z = ID(3,:);
+ID = eye(3, 3);
+ID_x = ID(1, :);
+ID_y = ID(2, :);
+ID_z = ID(3, :);
 
-quiver3(O(1,:), O(2,:), O(3,:), ID_x, ID_y, ID_z, 'k'); % plot origin
+quiver3(O(1, :), O(2, :), O(3, :), ID_x, ID_y, ID_z, 'k'); % plot origin
 grid on
 daspect([1 1 1])
 hold on
 
+SINGLE_BASESTATION = false;
+% SINGLE_BASESTATION = true;
+
+RAYS_FAR_APART = false;
+RAYS_FAR_APART = true;
+
+% Base Station positions in World Frame: (in Meters)
 B_1 = [-1.789562; 5.251678; 2.641019];
 B_2 = [1.734847; -4.475452; 2.665298];
-B_3 = [-1.789562; -4.475452; 2.665298];
+B_3 = [-1.759562; -4.505452; 2.635298];
 
+if SINGLE_BASESTATION
+    B_2 = B_1; % single base station
+    B_3 = B_1; % single base station
+end
 
 plot3(B_1(1), B_1(2), B_1(3), 'b^-'); % plot Base Station
 text(B_1(1), B_1(2), B_1(3), '1', 'Color', 'g')
@@ -27,79 +38,69 @@ text(B_2(1), B_2(2), B_2(3), '2', 'Color', 'r')
 plot3(B_3(1), B_3(2), B_3(3), 'b^-'); % plot Base Station
 text(B_3(1), B_3(2), B_3(3), '3', 'Color', 'r')
 
-
 % Artibary point on a Ray in World Frame (in Meters), to generate the Ray Vector
+% Rp_1 = [- 0.1; - 0.1; 1 - 0.1];
+% Rp_2 = [+ 0.1; + 0.1; 1 + 0.1];
 
-S_1 = [-0.1; -0.1; 1-0.1];
-S_2 = [+0.1; +0.1; 1+0.1];
-S_3 = [+0.1; -0.1; 1-0.1];
+Rp_1 = [- 0.1; - 0.1; 1 - 0.1];
 
-u = (S_1 - B_1) / norm(S_1 - B_1);
-v = (S_2 - B_2) / norm(S_2 - B_2);
-g = (S_3 - B_3) / norm(S_3 - B_3);
+if RAYS_FAR_APART
+    Rp_2 = Rp_1 + [0.2; 0 ; 0];
+    Rp_3 = Rp_1 + [0.1; 0.1 ; 0.1];
+else
+    Rp_2 = Rp_1 + [0.03; 0 ; 0];
+    Rp_3 = Rp_1 + [0.0075; 0.0075 ; 0.0075];
+end
 
-R_1 = 10 * u;
-R_2 = 10 * v;
-R_3 = 10 * g;
+plot3(Rp_1(1), Rp_1(2), Rp_1(3), 'k.-'); % plot Artibary point on Ray
+plot3(Rp_2(1), Rp_2(2), Rp_2(3), 'k.-'); % plot Artibary point on Ray
+plot3(Rp_3(1), Rp_3(2), Rp_3(3), 'k.-'); % plot Artibary point on Ray
+
+% Unit Vector of Ray from Base Stations
+u = (Rp_1 - B_1) / norm(Rp_1 - B_1);
+v = (Rp_2 - B_2) / norm(Rp_2 - B_2);
+g = (Rp_3 - B_3) / norm(Rp_3 - B_3);
+
+% Simulated End of Ray
+ray_length = 10;
+R_1 = ray_length * u;
+R_2 = ray_length * v;
+R_3 = ray_length * g;
 
 quiver3(B_1(1), B_1(2), B_1(3), R_1(1), R_1(2), R_1(3), 'b'); % plot Rays
 quiver3(B_2(1), B_2(2), B_2(3), R_2(1), R_2(2), R_2(3), 'b');
 quiver3(B_3(1), B_3(2), B_3(3), R_3(1), R_3(2), R_3(3), 'b');
 
+% distance between Base Stations
 fprintf('Distance Between BS 2 & 1: %f\n', norm(B_2 - B_1));
 fprintf('Distance Between BS 3 & 1: %f\n', norm(B_3 - B_1));
 fprintf('Distance Between BS 3 & 2: %f\n', norm(B_2 - B_3));
 
-%% Shortest Segment between two Rays
+%% 2 Base Stations, 1 Sensor (Find Shortest Segment between Rays)
+
 
 [Sc_1, Sc_2, d_c, k, d_c_k] = shortest_segment(B_1, u, B_2, v);
-fprintf('Shortest Distance Between Rays from BS 2 & 1: %f\n', d_c);
-    
-%% Calc
+fprintf('Shortest Distance Between Rays from BS 1 & 2: %f\n', d_c);
+plot3(Sc_1(1), Sc_1(2), Sc_1(3), 'b.-', Sc_2(1), Sc_2(2), Sc_2(3), 'b.-'); % plot Shortest Segment points
+quiver3(Sc_1(1), Sc_1(2), Sc_1(3), d_c_k(1), d_c_k(2), d_c_k(3), 'b'); % plot Shortest Segment vector
+
+[Sc_1, Sc_2, d_c, k, d_c_k] = shortest_segment(B_1, u, B_3, g);
+fprintf('Shortest Distance Between Rays from BS 1 & 3: %f\n', d_c);
+plot3(Sc_1(1), Sc_1(2), Sc_1(3), 'b.-', Sc_2(1), Sc_2(2), Sc_2(3), 'b.-'); % plot Shortest Segment points
+quiver3(Sc_1(1), Sc_1(2), Sc_1(3), d_c_k(1), d_c_k(2), d_c_k(3), 'b'); % plot Shortest Segment vector
+
+[Sc_1, Sc_2, d_c, k, d_c_k] = shortest_segment(B_2, v, B_3, g);
+fprintf('Shortest Distance Between Rays from BS 2 & 3: %f\n', d_c);
+plot3(Sc_1(1), Sc_1(2), Sc_1(3), 'b.-', Sc_2(1), Sc_2(2), Sc_2(3), 'b.-'); % plot Shortest Segment points
+quiver3(Sc_1(1), Sc_1(2), Sc_1(3), d_c_k(1), d_c_k(2), d_c_k(3), 'b'); % plot Shortest Segment vector
+
+% since Rays from Base Stations should surely touch the Sensor at the same
+% point, the distance here should be zero.
+% the extra distance may signify incorrect Base Station origins or lack of
+% calibration
 
 
-d_S = norm(S_2 - S_1);
-q = (S_2 - S_1) / norm(S_2 - S_1);
-d_S_q = d_S*q;
-% D = d_S*q;
-D = S_2 - S_1;
-
-fprintf('Assumed Distance Between Sensors: %f\n', d_S);
-% fprintf('Sensor Vector: [%f, %f, %f]\n', q(1), q(2), q (3));
-fprintf('Sensor Vector: \n');
-disp(D);
-
-d_BS1 = norm(S_1 - B_1);
-d_BS2 = norm(S_2 - B_2);
-fprintf('Sensor_1 Distance from BS_1: %f\n', d_BS1);
-fprintf('Sensor_2 Distance from BS_2: %f\n', d_BS2);
-
-% Sp_1 = B_1 + d_BS1*u
-% Sp_2 = B_2 + d_BS2*v
-
-w_0 = B_2 - B_1;
-
-s_n = d_BS1;
-% t_n = v\(d_S*q + s_n*u - w_0);
-% t_n = mldivide(v, (d_S*q + s_n*u - w_0));
-% t_n = (d_S*q + s_n*u - w_0).' * v ;
-t_n = (D + s_n*u - w_0).' * v; 
-    
-Sn_1 = B_1 + s_n*u;
-Sn_2 = B_2 + t_n*v;
-
-
-
-%% override
-
-% d_BS1 = norm(Sc_1 - B_1)
-% d_S = d_c
-% q = k
-
-%% Plot Shortest Segment between Rays
-
-plot3(Sc_1(1), Sc_1(2), Sc_1(3), 'c.-', Sc_2(1), Sc_2(2), Sc_2(3), 'c.-');
-quiver3(Sc_1(1), Sc_1(2), Sc_1(3), d_c_k(1), d_c_k(2), d_c_k(3), 'c');
+%%  2 Base Stations on 2 different Sensors (Best Fit of Segment between Rays)
 
 %% Plot Calc
 
@@ -218,9 +219,9 @@ a = (m*v - u);
 b = (c*v - D_w);
 
 % v_error = s_f*(m*v - u) + (c*v - D_w); % linear equation
-% v_error = s_f*a + b; % linear equation
+v_error = s_f*a + b; % linear equation
 
-% s_no_error_linear = - (m*v - u) \ (c*v - D_w); % assuming v_error = 0, find s_f
+s_no_error_linear = - (m*v - u) \ (c*v - D_w); % assuming v_error = 0, find s_f
 
 % v_error_sequared = power( norm(s_f*a + b), 2); % quadratic equation for reference only, to show derivation
 % v_error_sequared_diffed = 2*a*(a*s_f + b); %squared and differentiated by s_f
@@ -237,6 +238,6 @@ b = (c*v - D_w);
 
 %% Plot End
 
-% legend({'B_1', 'B_2', 'S_1', 'S_2', 'Sa_1', 'Sa_2'});
+% legend({'B_1', 'B_2', 'Rp_1', 'Rp_2', 'Sa_1', 'Sa_2'});
 
 hold off
