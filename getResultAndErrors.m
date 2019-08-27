9eda4ed225934e2223602f0a4faef857dc01b23b
NicksonYap <hi@nickson.me>
2019-08-28 05:45:10 +0800

    getResultAndErrors

---
+++ getResultAndErrors.m
@@ -0,0 +1,6 @@
+
+function [t_f, Rf_1, Rf_2, d_n_k, v_error, error] = getResultAndErrors(B_1, u, B_2, v, s_f, D_21)
+    [Rf_1, Rf_2, t_f, d_n_k, m_21, c_21, Dw_21] = getComponents(B_1, u, B_2, v, s_f, D_21);
+    v_error = s_f*(m_21*v - u) + (c_21*v - Dw_21);
+    error = power( norm( v_error ), 2);
+end
--- lighthousese3.m
+++ lighthousese3.m
@@ -128,11 +128,7 @@ for i=1:increments
 
     s_f = norm(Rp_1 - B_1) + increment;
     
-    [Rf_1, Rf_2, t_f, d_n_k, m_21, c_21, Dw_21] = getComponents(B_1, u, B_2, v, s_f, D_21);
-    
-    v_error = s_f*(m_21*v - u) + (c_21*v - Dw_21);
-    
-    error = power( norm( v_error ), 2);
+    [t_f, Rf_1, Rf_2, d_n_k, v_error, error] = getResultAndErrors(B_1, u, B_2, v, s_f, D_21);
     
     
     fprintf('%f \t %f \t %f \t %f \t %f \t %f \t %f \n', increment, s_f, t_f, error, v_error(1), v_error(2), v_error(3));
