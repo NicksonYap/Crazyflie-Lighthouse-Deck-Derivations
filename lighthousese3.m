982d5d8965d207b2e6fc98e9724881828788a372
NicksonYap <hi@nickson.me>
2019-08-29 19:42:45 +0800

    Rays on Sensor

--- lighthousese3.m
+++ lighthousese3.m
@@ -1,5 +1,5 @@
 
-%% Setup
+%% Basestation Configuration
 
 O = zeros(3, 3);
 
@@ -16,13 +16,12 @@ hold on
 SINGLE_BASESTATION = false;
 % SINGLE_BASESTATION = true;
 
-RAYS_FAR_APART = false;
-RAYS_FAR_APART = true;
 
 % Base Station positions in World Frame: (in Meters)
 B_1 = [-1.789562; 5.251678; 2.641019];
 B_2 = [1.734847; -4.475452; 2.665298];
 B_3 = [-1.759562; -4.505452; 2.635298];
+% B_3 = [-1.759562; -2.005452; 2.635298];
 
 if SINGLE_BASESTATION
     B_2 = B_1; % single base station
@@ -38,18 +37,93 @@ text(B_2(1), B_2(2), B_2(3), '2', 'Color', 'r')
 plot3(B_3(1), B_3(2), B_3(3), 'b^-'); % plot Base Station
 text(B_3(1), B_3(2), B_3(3), '3', 'Color', 'r')
 
-% Artibary point on a Ray in World Frame (in Meters), to generate the Ray Vector
-% Rp_1 = [- 0.1; - 0.1; 1 - 0.1];
-% Rp_2 = [+ 0.1; + 0.1; 1 + 0.1];
 
-Rp_1 = [- 0.1; - 0.1; 1 - 0.1];
+%%  Tracker Sensor Configuration
 
-if RAYS_FAR_APART
-    Rp_2 = Rp_1 + [0.2; 0 ; 0];
-    Rp_3 = Rp_1 + [0.1; 0.1 ; 0.1];
+% Sensor Positions Relative to Tracker
+
+sd_x = 3 / 100; % x-distance between sensors in Meters
+sd_y = 1.5 / 100; % y-distance between sensors in Meters
+
+S = [];
+S = [S, [- sd_x / 2; sd_y / 2; 0]]; % Sensor 0
+S = [S, [- sd_x / 2; - sd_y / 2; 0]]; % Sensor 1
+S = [S, [sd_x / 2; sd_y / 2; 0]]; % Sensor 2
+S = [S, [sd_x / 2; - sd_y / 2; 0]]; % Sensor 3
+
+for i = 1 : size(S(), 2)
+    Si = S(:, i);
+    plot3(Si(1), Si(2), Si(3), 'k.-'); % plot sensors
+    text(Si(1), Si(2), Si(3), num2str(i - 1), 'Color', 'k')
+end
+
+% simulate D vector as if Rays fall directly on both of Sensors (ignores sensor dimensions)
+
+
+P_sf = [0; 0; 1];
+
+yaw = 0; % degrees
+pitch = 0;
+roll = 0;
+rot_mat = angle2dcm(deg2rad(yaw), deg2rad(pitch), deg2rad(roll));
+
+sensor_on_ray_1 = 0;
+sensor_on_ray_2 = 2;
+sensor_on_ray_3 = 1;
+
+sensor_vector_21 = (S(:, sensor_on_ray_2 + 1) - S(:, sensor_on_ray_1 + 1));
+sensor_vector_31 = (S(:, sensor_on_ray_3 + 1) - S(:, sensor_on_ray_1 + 1));
+
+
+D_21 = rot_mat * sensor_vector_21  ;
+D_31 = rot_mat * sensor_vector_31  ;
+ 
+% D = D*1.1; % introduce errors by scaling
+fprintf('Assumed Distance Between Sensors at BS2 Ray & BS1 Ray: %f\n', norm(D_21));
+fprintf('Sensor Vector: \n');
+disp(D_21);
+
+quiver3(P_sf(1), P_sf(2), P_sf(3), D_21(1), D_21(2), D_21(3), 'r'); % plot D_21
+
+fprintf('Assumed Distance Between Sensors at BS3 Ray & BS1 Ray: %f\n', norm(D_31));
+fprintf('Sensor Vector: \n');
+disp(D_31);
+
+quiver3(P_sf(1), P_sf(2), P_sf(3), D_31(1), D_31(2), D_31(3), 'r'); % plot D_31
+    
+
+%% Rays Setup
+
+RAYS_ON_SENSOR = true
+% RAYS_ON_SENSOR = false
+
+if RAYS_ON_SENSOR
+    
+    % Artibary point on a Ray in World Frame (in Meters), to generate the Ray Vector
+
+    Rp_1 = P_sf;
+    Rp_2 = P_sf + D_21;
+    Rp_3 = P_sf + D_31;
+    
 else
-    Rp_2 = Rp_1 + [0.03; 0 ; 0];
-    Rp_3 = Rp_1 + [0.0075; 0.0075 ; 0.0075];
+
+    % Artibary point on a Ray in World Frame (in Meters), to generate the Ray Vector
+    % Rp_1 = [- 0.1; - 0.1; 1 - 0.1];
+    % Rp_2 = [+ 0.1; + 0.1; 1 + 0.1];
+
+    Rp_1 = [- 0.1; - 0.1; 1 - 0.1];
+
+    RAYS_FAR_APART = false;
+    RAYS_FAR_APART = true;
+
+    if RAYS_FAR_APART
+        Rp_2 = Rp_1 + [0.2; 0 ; 0];
+        Rp_3 = Rp_1 + [0.1; 0.1 ; 0.1];
+    else
+        Rp_2 = Rp_1 + [0.03; 0 ; 0];
+        Rp_3 = Rp_1 + [0.0075; 0.0075 ; 0.0075];
+    end
+
 end
 
 plot3(Rp_1(1), Rp_1(2), Rp_1(3), 'k.-'); % plot Artibary point on Ray
@@ -100,20 +174,6 @@ quiver3(Sc_1(1), Sc_1(2), Sc_1(3), d_c_k(1), d_c_k(2), d_c_k(3), 'b'); % plot Sh
 % calibration
 
 %%  2 Base Stations on 2 different Sensors (Best Fit of Segment between Rays)
-%% Calc
-
-% re-use the points for array as Tracker sensor detections
-% so the error should be zero
-
-D_21 = Rp_2 - Rp_1;
-fprintf('Assumed Distance Between Sensors at BS2 Ray & BS1 Ray: %f\n', norm(D_21));
-fprintf('Sensor Vector: \n');
-disp(D_21);
-
-D_31 = Rp_3 - Rp_1;
-fprintf('Assumed Distance Between Sensors at BS3 Ray & BS1 Ray: %f\n', norm(D_31));
-fprintf('Sensor Vector: \n');
-disp(D_31);
 
 
 %% Plot Calc
@@ -121,7 +181,7 @@ disp(D_31);
 step_size = 0.05;
 offset = 0;
 offset = -step_size/2;
-increments = 40;
+increments = 80;
 for i=1:increments
 %     disp(i)
     increment = (i-increments/2)*step_size + offset;
