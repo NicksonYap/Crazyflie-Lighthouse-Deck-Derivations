a0da22681f1101ed8b29f62232498028f22a4416
NicksonYap <hi@nickson.me>
2019-08-31 03:55:57 +0800

    planeRot

--- Helper.m
+++ Helper.m
@@ -8,9 +8,43 @@ classdef Helper
             B = inv(A_t*A)*A_t;
         end
         
+        function R = planeRot(a_x, a_y, b_x, b_y)
+            % ref: https://math.stackexchange.com/a/1876662/699426
+            
+            
+            % checks:
+            
+            % floating point equality ref: https://www.mathworks.com/help/matlab/ref/eq.html
+            tol = eps(0.5);
+            
+            if abs(norm(a_x) - norm(b_x)) >= tol || abs(norm(a_y) - norm(b_y)) >= tol
+                error('Magnitudes mismatch')
+            end
+            
+            if abs( dot(a_x, a_y) - dot(b_x, b_y)) >= tol 
+                error('Dot products mismatch')
+            end
+            
+            if abs( norm(cross(a_x, a_y)) - norm(cross(b_x, b_y)) ) >= tol 
+                error('Magnitude of cross products mismatch')
+            end
+            
+            a_z = cross(a_y, a_y); % might be the same if cross(a_x, a_x)
+            b_z = cross(b_y, b_y); % might be the same if cross(b_x, b_x)
+            
+            A = [a_x, a_y, a_z];
+            B = [b_x, b_y, b_z];
+            
+            R = B*pinv(A);
+            
+        end
+        
         function R = vectorRot(a, b)
             % ref: https://math.stackexchange.com/a/2161631
             % assuming a & b are unit vectors
+            
+%             R = Helper.planeRot(a, zeros(3, 1), b, zeros(3, 1) ); %equivalent, only if norm(a) == norm(b)
+%             return
 
             u = a/norm(a);                      % a and b must be column vectors
             v = b/norm(b);                      % of equal length
